fb43b1b873f3792d49dff5902c89effb17d52d7a
Berk Canpolat <canpolatberk19@gmail.com>
2026-05-16 18:22:44 +0300

    add responsive mobile menu with smooth animation

--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { navLinks } from './Navbar.data'
+import { CiMenuBurger } from "react-icons/ci";
+
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState(false);
+
   return (
     <header className='w-[50%] m-auto bg-[linear-gradient(rgb(33,33,33)_0%,rgb(20,20,20)_100%)] rounded-4xl py-4 px-5 border-5 border-[#242424] mt-5'>
+        <div className='flex flex-col md:flex-row md:items-center md:justify-between relative'>
         <div className='flex items-center justify-between'>
-        <div>
             <a href="https://www.wallofportfolios.in/portfolios/viha-shah/">
-            <h1 className='text-white font-extrabold'>DESIGNED BY VIHA</h1>
+            <h1 className='text-white font-medium text-sm md:text-[16px] md:font-extrabold'>DESIGNED BY VIHA</h1>
             </a>
+        <CiMenuBurger className='text-white md:hidden text-xl' onClick={() => setMenu((prev) => !prev)}/>
         </div>
-        <nav>
-            <ul className='list-none flex items-center gap-2.5 cursor-pointer'>
+        <nav className={`${menu ? "max-md:max-h-30 max-md:mt-6" : "max-md:max-h-0"} overflow-hidden md:overflow-visible md:block transition-all duration-700`}>
+            <ul className='flex flex-col gap-2 md:list-none md:flex-row md:items-center md:gap-2.5 md:cursor-pointer'>
                 {navLinks.map((item) => (
-                    <li key={item.id} className='text-gray-400 hover:text-white transition-all duration-500 font-extralight'>{item.title}</li>
+                    <li key={item.id} className='text-gray-400 hover:text-white transition-all duration-500 font-extralight text-sm md:text-[16px]'>{item.title}</li>
                 ))}
             </ul>
         </nav>
