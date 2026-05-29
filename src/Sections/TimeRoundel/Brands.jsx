b08f22b13635423152ca88dfbd63b9ac63dd0c3e
Berk Canpolat <canpolatberk19@gmail.com>
2026-05-29 14:42:41 +0300

    brand mobile responsive add

--- src/Sections/TimeRoundel/Brands.jsx
+++ src/Sections/TimeRoundel/Brands.jsx
@@ -4,9 +4,9 @@ import { IMAGEBRANDS } from '../../Constants/images.data'
 const Brands = () => {
   return (
     <div>
-        <h1 className='uppercase my-10 text-[rgb(161,161,161)] font-medium text-center'>partner brands | worked with</h1>
+        <h1 className='uppercase my-15 md:my-10 text-[rgb(161,161,161)] font-medium text-center'>partner brands | worked with</h1>
 
-        <div className='flex items-center gap-30'>
+        <div className='flex flex-col md:flex-row items-center gap-20 md:gap-30'>
             {IMAGEBRANDS.map((items,i) => (
                 <img src={items.brandsArray} className='w-22 h-full object-cover' />
             ))}
